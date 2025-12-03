bb35ff37f0f6976dc43d08f1dbbdd4e87b617df2
jelle <jelle@JELLEPC>
2025-12-03 17:54:32 +0100

    Add project files.

---
+++ WebApplication1/Controllers/GastController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Model;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GastController : Controller
+    {
+        [HttpGet("{id}")]
+        public ActionResult<Gast> GetGast(int id)
+        {
+            return new Gast
+            {
+                GastID = id,
+                VoorNaam = "Jan",
+                Achternaam = "Jansen",
+                Email = "jan@example.com",
+                Telefoon = "0612345678",
+                Adres = "Dorpsstraat 1",
+                Postcode = "1234AB",
+                Woonplaats = "Amsterdam",
+                Land = "NL",
+                Opmerking = "VIP-klant"
+            };
+        }
+
+        [HttpPost]
+        public ActionResult<Gast> CreateGast([FromBody] Gast gast)
+        {
+            return Ok(gast);
+        }
+    }
+}
---
+++ WebApplication1/Model/Gast.cs
@@ -0,0 +1,24 @@
+﻿namespace WebApplication1.Model
+{
+    public class Gast
+    {
+        public int GastID { get; set; }
+        public string VoorNaam { get; set; }
+        public string Achternaam { get; set; }
+        public string Email { get; set; }
+        public string Telefoon { get; set; }
+        public string Adres { get; set; }
+        public string Postcode { get; set; }
+        public string Woonplaats { get; set; }
+        public string Land { get; set; }
+        public string Opmerking { get; set; }
+
+        public string GetVolledigeNaam()
+            => $"{VoorNaam} {Achternaam}";
+
+        public bool IsContactGegevensCompleet()
+            => !string.IsNullOrWhiteSpace(Email) &&
+               !string.IsNullOrWhiteSpace(Telefoon);
+
+    }
+}
